81cc1fbebf4d4bcff4859f4f7fb0bb04ab586c12
Ivan1998-23 <birjukivan@gmail>
2023-08-31 04:05:03 -0400

    flask start

--- python_nmap/exam_fping.py
+++ python_nmap/exam_fping.py
@@ -1,35 +1,23 @@
 import sys
 import subprocess
 import csv
-
-address = sys.argv[1]
+from datetime import date
 
 #функция пингует хосты и дописывает активные хосты в файл output_scv.csv
 #обязательно вписывать адрес с маской через /
 # Пример вызова ф-и :   python exam_fping.py 192.168.100.1/28
-def fping_all(address):
-    '''
-    command = 'fping' 
-   # result = subprocess.run([command, '-g', '-c', '4',  address], stdout = subprocess.DEVNULL, encoding = 'utf-8')
-    result = subprocess.run([command, '-c', '4', address])
-    print([command,address])
-    print(result.stdout)
-    with open('output.txt', 'a') as f:
-        print(result.stdout, file=f)
-    '''
+def fping_all(address): 
+    today = date.today()
+    write_file = 'result_scan/' + str(today) + '.txt' 
+    result = subprocess.run(['fping', '-a', '-g',  address], stdout = subprocess.PIPE)
     
-    result = subprocess.run(['fping', '-g',  address], stdout = subprocess.PIPE)
-    
-    mas = result.stdout.decode('utf-8').split('\n')
-    mas_alive = []
-    for element in mas:
-        if 'alive' in element:
-            mas_alive.append(element.split()[0])
-    
-    with open('output_in_txt.txt', 'a') as f2:
+    mas_alive = result.stdout.decode('utf-8').split('\n') 
+  
+    with open(write_file, 'a') as f2:
         for row in mas_alive:
             print(row, file=f2)
-     
+        print('', file=f2)
           
 if __name__ == '__main__':
-    fping_all(address)
+	address = sys.argv[1] 
+	fping_all(address)
--- python_nmap/scan_nmap.py
+++ python_nmap/scan_nmap.py
@@ -1,17 +1,52 @@
 # pip install python-nmap
 import nmap
+import csv
+from datetime import date
+import subprocess
+ 
+
+ports_from_server = ["22","80", "443", "5060", "5062", "8080"]
+ports_from_windows = ["3389", "25", "445", "1433", "53", "67", "68", "161", "162", "139", "135"]
 
 
-def scan_network(network_range):
-    nm = nmap.PortScanner()
-    nm.scan(hosts=network_range, arguments='-p 22')  # Scan ports 22 to 443
+def scan_nmap_write_csv(network_range):
+	today = date.today()
+	save_file_from_csv = 'result_scan/csv_' + str(today) + '.csv'
+	port = ', '.join(ports_from_server)
+	nm = nmap.PortScanner()
+	nm.scan(hosts=network_range, ports= port, arguments='-sS')  # Scan ports 80 to 443
+	
+	save_csv = nm.csv()
+	host = nm.all_hosts()[0]
+	hosts = nm.all_hosts() 
+	scan_results = []
+	input_data = []
+	
+	with open(save_file_from_csv, mode="a", newline="", encoding="utf-8") as file:
+		file.write(save_csv[96::])
+	'''
     print(nm.command_line())
-    print(nm.scaninfo())
-    print(nm.csv())
-    # for host in nm.all_hosts():
-    #     print(f"Host: {host}")
-    #     print(f"  State: {nm[host].state()}")
-    #     print(f"  Ports: {nm[host]['tcp'].keys()}")
-
-
-
+    print(nm.scaninfo()) 
+    print(save_csv)
+    print( nm.all_hosts()) 
+    print(nm[host].hostname()) 
+    print(nm[host].state()) 
+    print(nm[host].all_protocols()) 
+    print(nm[host].has_tcp(22))  
+'''
+def scan_network(network_range):
+	today = date.today()
+	port = ', '.join(ports_from_windows)
+	port2 = ', '.join(ports_from_server)
+	write_file = 'result_scan/nmap_' + str(today) + '.txt'
+	result = subprocess.run(['nmap', '-p', f'{port2}',   network_range], stdout = subprocess.PIPE, encoding='utf-8')
+	
+	
+	with open(write_file, 'w') as f2:
+		print(result.stdout, file=f2)
+	print(result.stdout)
+   
+if __name__ == '__main__':
+	network = '192.168.1.0/24'
+	scan_network(network)
+	
